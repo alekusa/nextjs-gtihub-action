26a38d7060d59cc87840e7ecffc326db1668c171
AleKusa <alekusa@gmail.com>
2022-11-13 19:47:04 -0300

    Por Herror en br - update

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Trabajando con GitHub Actions<br> Update - Actions</br>
+          Trabajando con GitHub Actions Update - Actions
         </h1>
 
         <p className={styles.description}>
